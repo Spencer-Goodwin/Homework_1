a56ac24c416be7be654c20fae664461e40b03dca
Spencer.Goodwin <Spencer.Goodwin@bison.howard.edu>
2017-01-26 22:35:10 -0500

    problem 3 changes

--- Homework_1.rkt
+++ Homework_1.rkt
@@ -19,13 +19,11 @@
          (+ (car L) (sum-up-numbers-simple (cdr L)))) ((sum-up-numbers-simple (cdr L)))))
 ;//Function number three
 (define (sum-up-numbers-general L)
-  (cond  ((null? L) 0)
-         (((not(list? L))sum-up-numbers-general(car L))
-          ((null? L) 0
-        
-         
-         (sum-up-numbers-simple(car L)))
-    ((not(null? L)sum-up-numbers-simple L))))
+  (cond ((not(list? L))(sum-up-numbers-general(car L)))
+        ((null? L) 0)
+         ((number? (car L))
+         (+ (car L) (sum-up-numbers-general (cdr L)))((sum-up-numbers-general (cdr L))))
+         (else (sum-up-numbers-general(car L)))))
         
              
         
